c6c4e40df7a35c0130df52aca87e491d7cc578c9
mhb <mhb@dossier.us>
2025-01-31 13:23:19 -0500

    Sandbox

---
+++ echo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eo pipefail
+FUNCTION=$(aws cloudformation describe-stack-resource --stack-name java17-examples --logical-resource-id function2 --query 'StackResourceDetail.PhysicalResourceId' --output text)
+while true; do
+  aws lambda invoke \
+      --function-name $FUNCTION \
+      --cli-binary-format raw-in-base64-out \
+      --payload '{ "uglyXML": "<XmlBallot><event>ReplacementCon</event><election>In the Know Award Finals</election><castAt>2025-01-24T17:11:23.625911-05:00</castAt><memberUUID>9cb15cdb-3f4e-4722-852b-0c51f155cfd0</memberUUID><category><name>Best Novel</name><vote>The Integral Trees by Larry Niven</vote></category><category><name>Best Novelette</name><vote>“Winterfair Gifts” by Lois McMaster Bujold</vote><vote>“The Concrete Jungle” by Charles Stross</vote></category><category><name>Best Novella</name><vote>“Elemental” by Geoffrey A. Landis</vote></category><category><name>Best Short Story</name><vote>“Biographical Notes to ‘A Discourse on the Nature of Causality, with Air-Planes’ by Benjamin Rosenbaum”</vote></category></XmlBallot>" }' \
+      out.xml
+  cat out.xml
+  echo ""
+  sleep 2
+done
---
+++ src/main/java/blockchain/HandleSmofML.java
@@ -0,0 +1,43 @@
+package blockchain;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.io.Writer;
+
+public class HandleSmofML  implements RequestHandler<SmofML, String> {
+    @Override
+    public String handleRequest(SmofML wrapper, Context context) {
+        try {
+            InputSource src = new InputSource(new StringReader(wrapper.uglyXML()));
+            Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(src);
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            transformerFactory.setAttribute("indent-number", 4);
+            Transformer transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+            Writer out = new StringWriter();
+            transformer.transform(new DOMSource(document), new StreamResult(out));
+            return out.toString();
+        } catch (IOException | ParserConfigurationException | TransformerException | SAXException oops) {
+            return oops.getMessage();
+        }
+    }
+}
+
+record SmofML(String uglyXML) {
+}
